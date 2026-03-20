50cd1c4ab893c59ef654a3bd6ed18f7354105d9b
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2026-03-20 15:14:46 -0300

    Metodo refaz

--- src/AEDS_II/heapsort/FPHeapMax.java
+++ src/AEDS_II/heapsort/FPHeapMax.java
@@ -8,5 +8,20 @@ public class FPHeapMax {
         this.v = v; this.n = this.v.length - 1;
     }
 
-
+    public void refaz(int esq, int dir){
+        int j = esq * 2;
+        Item x = this.v[esq];
+        while(j <= dir){
+            if((j < dir) && (this.v[j].compara(this.v[j+1]) < 0)){
+                j++;
+            }
+            if(x.compara(this.v[j])>= 0){
+                break;
+            }
+            this.v[esq] = this.v[j];
+            esq = j;
+            j = esq*2;
+        }
+        this.v[esq] = x;
+    }
 }
